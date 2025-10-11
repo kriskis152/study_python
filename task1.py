28d2d14a27b355215d5ed32e867e48442322ccb2
Kristina Soboleva <kriskis152s@gmail.com>
2025-10-12 01:16:27 +0300

    Tasks 1 to 4

---
+++ task1.py
@@ -0,0 +1,7 @@
+a = int(input("Введите возраст: "))
+if a < 18 :
+ print("Ребенок")
+elif a >= 18 and a < 65 :
+ print("Взрослый")
+elif a >= 65 :
+ print("Пенсионер") 
---
+++ task2.py
@@ -0,0 +1,11 @@
+a = int(input("Введите балл: "))
+if a >= 90 and a<=100 :
+ print("Оценка A")
+elif a >=80 and a <=89 :
+ print("Оценка B")
+elif a >=70 and a <=79 :
+ print("Оценка C")
+elif a >=60 and a <=69 :
+ print("Оценка D")
+elif a >=0 and a <=59 :
+ print("Оценка F")
