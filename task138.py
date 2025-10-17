78f8f26a0c86b995c3adcec9e3bfe4aed6049593
Kristina Soboleva <kriskis152s@gmail.com>
2025-10-17 14:54:29 +0300

    not done

--- task138.py
+++ task138.py
@@ -1,4 +1,9 @@
 chislo = int(input("Введи число: "))
 
-
-
+for i in range(chislo) :
+    print("*", end="")
+print()    
+for i in range(chislo-2) :
+    print("*")
+for i in range(chislo) :
+    print("*", end="")
